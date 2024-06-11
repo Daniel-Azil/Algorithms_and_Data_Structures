03eb2e1500a43e38d6b17b00e526ec38adf717fa
Daniel Azil <danielazil.fse@gmail.com>
2024-06-11 11:15:30 +0000

    A module that retursn an array of strings, group by anagrams.

--- Arrays_and_Hashing/4-Group_Anagrams.py
+++ Arrays_and_Hashing/4-Group_Anagrams.py
@@ -1,3 +1,25 @@
+"""
+    Group Anagrams
+
+    Description:
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+    An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original letters exactly once.
+
+    Example 1:
+    Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+    Example 2:
+    Input: strs = [""]
+    Output: [[""]]
+
+    Example 3:
+    Input: strs = ["a"]
+    Output: [["a"]]
+"""
+
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
