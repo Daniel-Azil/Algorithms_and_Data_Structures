816f83e5efcbe307efc01820a2392549387afec6
Daniel Azil <danielazil.fse@gmail.com>
2024-06-11 11:09:32 +0000

    A module that returns true if t is an anagram of s, and false otherwise.

--- Arrays_and_Hashing/2-Valid_Anagram.py
+++ Arrays_and_Hashing/2-Valid_Anagram.py
@@ -1,3 +1,21 @@
+"""
+    Valid Anagram
+
+    Description:
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    An anagram is a word or phrase formed by rearranging the letters of another word or phrase, using all the original letters exactly once.
+
+    Example 1:
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+    Example 2:
+    Input: s = "rat", t = "car"
+    Output: false
+"""
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
