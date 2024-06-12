38e9f53c892777eff739a77e777a137c1b0ee2d2
Daniel Azil <danielazil.fse@gmail.com>
2024-06-12 17:39:46 +0000

    A method that reverses a linked list.

--- linked_list/1-reversed_linked_list.py
+++ linked_list/1-reversed_linked_list.py
@@ -1,3 +1,27 @@
+"""
+    Reverse a Linked List
+
+    Description:
+    Given the head of a singly linked list, reverse the list and return the reversed list.
+
+    Example 1:
+    Input: [1,2,3,4,5]
+    Output: [5,4,3,2,1]
+
+    Example 2:
+    Input: [1,2]
+    Output: [2,1]
+
+    Example 3:
+    Input: []
+    Output: []
+
+"""
+
+
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
